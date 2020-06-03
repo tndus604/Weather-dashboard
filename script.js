ba0011fc30e2f242b0c5d005d4ea681b6cc5de1e
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-06-03 16:35:42 -0400

    Added icons for 5-day forecast

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 async function fetchWeather () {
+    var rightRow = document.getElementById('rightRow');
+    rightRow.style.display = "block";
     var cityName = document.querySelector("#searchCity").value;
     var APIKey = "166a433c57516f51dfab1f7edaed8413";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid=" + APIKey;
@@ -46,7 +48,87 @@ function fetchUVIndex (latCity, lonCity) {
     }).then (displayUVIndex);
 }
 
-function displayUVIndex (response) {
-    console.log ( `[displayUVIndex]`, response )
-    document.querySelector('.uvIndex').textContent = "UV Index: " + response.value;
+function displayUVIndex (weatherData) {
+    console.log ( `[displayUVIndex]`, weatherData );
+    document.querySelector('.uvIndex').textContent = "UV Index: " + weatherData.value;
+    fetchFiveDays();
+}
+
+function fetchFiveDays () {
+    var cityName = document.querySelector("#searchCity").value;
+    var APIKey = "166a433c57516f51dfab1f7edaed8413";
+    var fiveDaysURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey;
+
+    $.ajax({
+        url: fiveDaysURL,
+        method:"GET"
+      }).then (displayFiveDays);
+}
+
+function displayFiveDays(weatherData) {
+    console.log ( `[displayFiveDays]`, weatherData );
+    
+    //Setting up the temperature for 5 days
+    var tempKel = weatherData.list[7].main.temp;
+    var tempCel = tempKel - 273.15;
+    document.querySelector('.fiveTemp1').textContent = "Temp: " + tempCel + "°C";
+
+    var tempKel = weatherData.list[15].main.temp;
+    var tempCel = tempKel - 273.15;
+    document.querySelector('.fiveTemp2').textContent = "Temp: " + tempCel + "°C";
+
+    var tempKel = weatherData.list[23].main.temp;
+    var tempCel = tempKel - 273.15;
+    document.querySelector('.fiveTemp3').textContent = "Temp: " + tempCel + "°C";
+
+    var tempKel = weatherData.list[31].main.temp;
+    var tempCel = tempKel - 273.15;
+    document.querySelector('.fiveTemp4').textContent = "Temp: " + tempCel + "°C";
+
+    var tempKel = weatherData.list[39].main.temp;
+    var tempCel = tempKel - 273.15;
+    document.querySelector('.fiveTemp5').textContent = "Temp: " + tempCel + "°C";
+
+    //Setting up the humidity for 5 days
+    document.querySelector('.fiveHumidity1').textContent = "Humidity: " + weatherData.list[7].main.humidity + "%";
+    document.querySelector('.fiveHumidity2').textContent = "Humidity: " + weatherData.list[15].main.humidity + "%";
+    document.querySelector('.fiveHumidity3').textContent = "Humidity: " + weatherData.list[23].main.humidity + "%";
+    document.querySelector('.fiveHumidity4').textContent = "Humidity: " + weatherData.list[31].main.humidity + "%";
+    document.querySelector('.fiveHumidity5').textContent = "Humidity: " + weatherData.list[39].main.humidity + "%";
+
+    //Setting up the icon for 5 days
+    var iconImg = "http://openweathermap.org/img/w/" + weatherData.list[7].weather[0].icon + ".png";
+
+    var cityImg = $("<img>");
+    cityImg.attr("src", iconImg);
+    $('.fiveIcon1').append(cityImg);
+
+    var iconImg = "http://openweathermap.org/img/w/" + weatherData.list[15].weather[0].icon + ".png";
+
+    var cityImg = $("<img>");
+    cityImg.attr("src", iconImg);
+    $('.fiveIcon2').append(cityImg); 
+
+    var iconImg = "http://openweathermap.org/img/w/" + weatherData.list[23].weather[0].icon + ".png";
+
+    var cityImg = $("<img>");
+    cityImg.attr("src", iconImg);
+    $('.fiveIcon3').append(cityImg); 
+
+    var iconImg = "http://openweathermap.org/img/w/" + weatherData.list[31].weather[0].icon + ".png";
+
+    var cityImg = $("<img>");
+    cityImg.attr("src", iconImg);
+    $('.fiveIcon4').append(cityImg); 
+
+    var iconImg = "http://openweathermap.org/img/w/" + weatherData.list[39].weather[0].icon + ".png";
+
+    var cityImg = $("<img>");
+    cityImg.attr("src", iconImg);
+    $('.fiveIcon5').append(cityImg); 
+
+    
+
+
+
 }
